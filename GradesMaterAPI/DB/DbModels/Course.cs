0926a193d104a16db5aebf9430f3ba72350b1ab0
Gur <silbermangur@gmail.com>
2024-06-02 15:26:48 +0300

    added the Course and Teacher models

---
+++ GradesMaterAPI/DB/DbModels/Course.cs
@@ -0,0 +1,13 @@
+﻿namespace GradesMaterAPI.DB.DbModels
+{
+    public class Course
+    {
+        int Id { get; set; }
+        public string CourseName { get; set; }
+        public string CourseDescription { get; set; }
+
+        public string TeacherId { get; set; } 
+
+        public Teacher Teacher { get; set; }  
+    }
+}
