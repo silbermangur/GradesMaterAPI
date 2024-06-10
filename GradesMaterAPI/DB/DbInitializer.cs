4bc4b7be97941c062172f3ac72a9d93fd48e3b19
Gur <silbermangur@gmail.com>
2024-06-10 13:23:47 +0300

    Create a global service to inilaze the dataBase and create dbInilazer file to inilaze a rows in the database

---
+++ GradesMaterAPI/DB/DbInitializer.cs
@@ -0,0 +1,27 @@
+﻿using GradesMaterAPI.DB.DbModels;
+
+namespace GradesMaterAPI.DB
+{
+    // static class - mean that all the function and variables are static.
+    public static class DbInitializer
+    {
+        // dbContext is like a seasson for the database
+        public static void Initialize(GradeMasterDbContext dbContext)
+        {
+            dbContext.Database.EnsureCreated();
+            // Any() - check if there any rows in the table
+            if(!dbContext.Teachers.Any())
+            {
+                var teacher = new DB.DbModels.Teacher[]
+            {
+                new Teacher {FirstName = "Albert", LastName="Einstein", Email="aa@gmail.com", PhoneNumber="0545545497", Password="12345"},
+                new Teacher {FirstName = "david", LastName="becham", Email="abb@gmail.com", PhoneNumber="0545789456", Password="12345"}
+            };
+                dbContext.Teachers.Add(teacher[0]);
+                dbContext.Teachers.Add(teacher[1]);
+                dbContext.SaveChanges();
+            }
+           
+        }
+    }
+}
--- GradesMaterAPI/Program.cs
+++ GradesMaterAPI/Program.cs
@@ -16,12 +16,32 @@ namespace GradesMaterAPI
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            // Register Global DB Service with EF
+            // when you want to work with the db so provide my with the GradeMasterDbContext where there is tables.
+            // Global Service - we can you use it the service from any where in the application
             builder.Services.AddDbContext<GradeMasterDbContext>(options =>
             {
                 options.UseSqlServer("Data Source=DESKTOP-PR461MF\\SQLEXPRESS;Initial Catalog=GradeMasterdb_EFDb;Integrated Security=True;Connect Timeout=30;");
             });
-            var app = builder.Build();
 
+            // TODO Add Onther Service Here....
+
+            var app = builder.Build();
+            using (var scope = app.Services.CreateScope())
+            {
+                try
+                {
+                    // Get Register GradeMasterDbContext Service
+                    var services = scope.ServiceProvider;
+                    var context = services.GetRequiredService<GradeMasterDbContext>();
+                    // Initialize db
+                    DbInitializer.Initialize(context);
+                } catch (Exception ex) 
+                {
+                    //TODO print to looger
+                }
+            }
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
